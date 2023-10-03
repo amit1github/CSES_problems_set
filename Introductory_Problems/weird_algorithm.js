0daee7c3a467070d4271aefeb28f87bb19c68a28
Amit Kumar Yadav <49604010+amit1github@users.noreply.github.com>
2023-10-03 11:51:32 +0530

    fix

--- Introductory_Problems/weird_algorithm.js
+++ Introductory_Problems/weird_algorithm.js
@@ -1,18 +1,18 @@
-import readline from "readline";
+const readline = require("readline")
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-rl.question("Enter a number: ", (input) => {
+rl.question("", (input) => {
   let n = parseInt(input, 10);
-
+    
   while (n !== 1) {
     process.stdout.write(n + " ");
 
     if (n % 2 === 0) {
-      n = n / 2;
+      n /= 2;
     } else {
       n = n * 3 + 1;
     }
